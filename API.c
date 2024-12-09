3a08aeb3ca32d05a29d1e97fc24bb1c09d1aa969
Chessco <80254156+Chessco13@users.noreply.github.com>
2024-12-09 06:35:18 +0100

    Add files via upload

---
+++ API.c
@@ -0,0 +1,20 @@
+// librairie qui recupere les données de la sonde//
+#include "crow_all.h" 
+
+// librairie qui se connecte a la base de donnée SQL//
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/prepared_statement.h>
+
+// librairie qui permet d'envoyer les donnée sur une interface html avec REST inio//
+#include <restinio/all.hpp> 
+
+
+// configuration de l'api//
+
+//Initialisation de l'api//
+
+//Récuperation des données du capteur//
+
+//connexion a la base de donnée//
+
